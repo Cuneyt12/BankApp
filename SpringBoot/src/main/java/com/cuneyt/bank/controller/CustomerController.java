e434b05551f05aecff6a7bccc31cc8d7d983f8b7
Cuneyt12 <cuneytokmenn@gmail.com>
2023-02-21 15:42:42 +0300

    Qualifier anotasyonu eklendi

--- SpringBoot/src/main/java/com/cuneyt/bank/controller/CustomerController.java
+++ SpringBoot/src/main/java/com/cuneyt/bank/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.cuneyt.bank.controller;
 
 import com.cuneyt.bank.entity.Customer;
 import com.cuneyt.bank.service.ICustomerService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/musteri")
 public class CustomerController {
+
     private final ICustomerService iCustomerService;
 
-    public CustomerController(ICustomerService iCustomerService) {
+    public CustomerController(@Qualifier("customerServiceImpl") ICustomerService iCustomerService) {
         this.iCustomerService = iCustomerService;
     }
 
--- SpringBoot/src/main/java/com/cuneyt/bank/service/impl/CustomerServiceImpl.java
+++ SpringBoot/src/main/java/com/cuneyt/bank/service/impl/CustomerServiceImpl.java
@@ -3,7 +3,9 @@ package com.cuneyt.bank.service.impl;
 import com.cuneyt.bank.entity.Customer;
 import com.cuneyt.bank.repository.ICustomerRepository;
 import com.cuneyt.bank.service.ICustomerService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomerServiceImpl implements ICustomerService {
 
     private final ICustomerRepository iCustomerRepository;
