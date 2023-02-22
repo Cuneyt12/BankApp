bc960a66efebfc7793771c0c639ac6c9534bd653
Cuneyt12 <56197698+Cuneyt12@users.noreply.github.com>
2023-02-22 12:22:08 +0300

    Delete ICustomerService.java

--- SpringBoot/src/main/java/com/cuneyt/bank/service/ICustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.cuneyt.bank.service;
-
-import com.cuneyt.bank.entity.Customer;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface ICustomerService {
-    String paraYatir(Customer customer, String banka);
-}
